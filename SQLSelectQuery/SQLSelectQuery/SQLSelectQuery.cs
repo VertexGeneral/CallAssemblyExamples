e146a5950ae6b9d3ccb7e69f58acad7709e408c5
VertexGeneral <swebb@square-9.com>
2016-04-15 15:22:33 -0400

    Revert "Updated to use Property Name instead of hard coded Property ID to find the connection string, sql statement, and return properties"
    This reverts commit fad26eb56a80ed89a23b09936934d94b716211f9.

--- SQLSelectQuery/SQLSelectQuery/SQLSelectQuery.cs
+++ SQLSelectQuery/SQLSelectQuery/SQLSelectQuery.cs
@@ -20,16 +20,11 @@ namespace SQLSelectQuery
             SqlConnection connection = new SqlConnection();
             SqlCommand command = new SqlCommand();
 
-            //Finds the Property ID for the Connection, SQL, and Return properties in GlobalAction
-            string connKey = Input.FirstOrDefault(x => x.Value == "Connection").Key;
-            string sqlKey = Input.FirstOrDefault(x => x.Value == "SQL").Key;
-            string returnKey = Input.FirstOrDefault(x => x.Value == "Return").Key;
+            //Property 1 is used as our SQL Connection String
+            connection.ConnectionString = @Input["1"];  //1 represents the ID of the property that contains the connection string.
 
-            //connKey is used as our SQL Connection String
-            connection.ConnectionString = @Input[connKey];  //conKey represents the ID of the property that contains the connection string.
-
-            //sqlKey is used as our SQL Query.
-            String sqlQuery = Input[sqlKey]; //sqlKey represents the ID of the property that contains the SQL Query.
+            //Property 2 is used as our SQL SELECT Query.
+            String sqlQuery = Input["2"]; //2 represents the ID of the property that contains the SQL Query.
             sqlQuery = sqlQuery.Replace("#ARCHIVEID#", Input["ARCHIVEID"]);
             sqlQuery = sqlQuery.Replace("#DOCID#", Input["DOCUMENTID"]);
             sqlQuery = sqlQuery.Replace("#DATABASEID#", Input["DATABASEID"]);
@@ -49,14 +44,10 @@ namespace SQLSelectQuery
                     {
                         if (reader[0] != null)
                         {
-                            //The result that is returned from the SQL query is added to our return Dictionary object as Return's value.
-                            Output.Add(returnKey, reader[0].ToString()); //returnKey represents the ID of the property that contains the return value.
+                            //The result that is returned from the SQL query is added to our return Dictionary object as Property 3's Value.
+                            Output.Add("3", reader[0].ToString()); //3 represents the ID of the property that contains the return value.
                         }
-                        else
-                            //Return "No data" if the return is empty. This allows EXEC statements to be passed through the assembly to execute stored procedures.
-                            Output.Add(returnKey, "No data");
                     }
-
                 }
                 connection.Close();
             }
